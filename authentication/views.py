036a1034cf42f6f453757d133a8c6c900971220d
Roberto <alonso.delacruzhdez@gmail.com>
2023-01-16 21:35:19 -0600

    adding postgresql

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your views here.
+# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length = 150, unique = True)
+    
+    
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["password"]
+    
+    def __str__(self):
+        return f"{self.email}"
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'restframework'
+
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +78,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+       'NAME': 'analitica_neg',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
